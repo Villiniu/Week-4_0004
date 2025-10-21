90d8233b6974a50335eca4bd69edb7fac95bdf60
Villiniu <villiniuprime@gmail.com>
2025-10-21 15:44:50 +0700

    Add input umur

--- learnvariable.java
+++ learnvariable.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class learnvariable {
     public static void main(String[] args) {
        int umur;
        String nama;
-        
+
+       Scanner newScan = new Scanner(System.in)
+       System.err.println("Masukan Umur = ");
+       umur = newScan.nextInt();
+       
     }
 }
